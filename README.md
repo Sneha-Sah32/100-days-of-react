705791c3f5799922c58be67f380b60cb05c22208
Sneha Sah <sahsneha2062@gmail.com>
2025-10-13 21:35:57 +0545

    fixed bugs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Running this project
+* Clone the repo to your device
+* "npm install" in your terminal to install necessary dependencies
+* Go to App.js and uncomment the component you wanna start with
+* "npm start" in your terminal
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/ShowHidetext/Showhide.jsx
+++ src/components/ShowHidetext/Showhide.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
 
 const Showhide = () => {
-    const [show,setShow]=useState(false);
+    const [hide,setHide]=useState(false);
     
     const handleClick=()=>{
-        setShow(!show);
+        setHide(!hide);
     }
 
-    const [text,setText]=useState("");
+    const [text,setText]=useState();
     
   return (
     <div>
-      <textarea name="textspace" id="txt" placeholder='Enter Your Text Here' onChange={(e)=>setText(e.target.value)} style={{marginTop:'18rem',marginLeft:'25rem',width:'20rem',height:"15rem"}}></textarea>
-      <button onClick={handleClick} style={{marginLeft:'2rem'}}>{show?"hide":"show"}</button>
+      
+      <textarea name="textspace" id="txt" placeholder='Enter Your Text Here' onChange={(e)=>setText(e.target.value)} value={hide===true?'':text} style={{marginTop:'18rem',marginLeft:'25rem',width:'20rem',height:"15rem"}}></textarea>
+
+      <button onClick={handleClick} style={{marginLeft:'2rem'}}>{hide?"show":"hide"}</button>
+      
     </div>
   )
 }
