186be52723fe565382a39c25967fb1d1723703f6
Sneha Sah <sahsneha2062@gmail.com>
2025-10-17 21:25:59 +0545

    made this component kinda cool maybe:

--- src/components/PrimeNumber/Prime.jsx
+++ src/components/PrimeNumber/Prime.jsx
@@ -33,9 +33,12 @@ const Prime = () => {
   }
   return (
     <div>
-      <input type="number" value={num} onChange={handleChange}/>
-      <button onClick={handleClick}>check</button>
-      <p>the given number is: {isprime}</p>
+      <h1>Check the prime number:</h1>
+    <div style={{margin:"4rem",backgroundColor:'black',height:'10rem',width:'20rem'}}>
+      <input type="number" value={num} onChange={handleChange} style={{marginLeft:'2rem',marginTop:'2rem',height:'1.5rem',backgroundColor:'lightgrey'}}/>
+      <button onClick={handleClick} style={{backgroundColor:'lightsteelblue',margin:'1rem',height:'2rem',width:'3.5rem',borderRadius:'4px'}}>check</button>
+      <p style={{marginLeft:'2rem',marginTop:'2rem',color:'yellowgreen'}}>{isprime?`The given number is:${isprime}`:"Enter a number:"}</p>
+    </div>
     </div>
   )
 }
